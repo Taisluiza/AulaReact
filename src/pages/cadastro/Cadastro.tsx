8a582c30c1e65c42888c15625a16168225111b57
Taisluiza <taissalissousa@gmail.com>
2023-08-03 18:05:46 -0300

    Consumo do Back End - CRUD de Postagens

--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -134,7 +134,7 @@ function Cadastro() {
             />
           </div>
           <div className="flex justify-around w-full gap-8">
-            <button className='rounded text-white bg-red-400 hover:bg-red-700 w-1/2 py-2' onClick={back}>
+            <button className='rounded text-white bg-range-400 hover:bg-red-700 w-1/2 py-2' onClick={back}>
               Cancelar
             </button>
             <button className='rounded text-white bg-indigo-400 hover:bg-indigo-900 w-1/2 py-2' type='submit'>
