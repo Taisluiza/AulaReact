ceb305277f8e8c6b0eac3ede688583f3d525c569
Taisluiza <taissalissousa@gmail.com>
2023-08-16 17:00:44 -0300

    Atualização

--- src/pages/perfil/Perfil.tsx
+++ src/pages/perfil/Perfil.tsx
@@ -16,13 +16,15 @@ function Perfil() {
   }, [usuario.token])
 
   return (
-    <div className='container mx-auto mt-4 rounded-2xl overflow-hidden'>
-      <img className='w-full h-80 object-cover border-b-4 border-white' src={loginLogo} alt="Capa do Perfil" />
+    <div className='className="flex w-full flex-row-reverse'>
+      <div className="w-2/12 h-screen fixed top-0 left-0 cursor-pointer">
+      <img className='w-full h-72 object-cover' src={loginLogo} alt="Capa do Perfil" />
       <img src={usuario.foto} alt={`Foto de perfil de ${usuario.nome}`} className='rounded-full w-56 mx-auto mt-[-8rem] border-8 border-white relative z-10' />
       <div className="relative mt-[-6rem] h-72 flex flex-col bg-rose-950 text-white text-2xl items-center justify-center">
         <p>Nome: {usuario.nome} </p>
         <p>Email: {usuario.usuario}</p>
       </div>
+    </div>
     </div>
   )
 }
